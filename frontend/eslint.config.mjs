bb2d1a6d73eb5b60dcd7cdbf57f34acdf1cc59ba
SheetalTaank12 <sheetaltaank74@gmail.com>
2026-02-18 00:01:10 +0530

    add

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -10,6 +10,11 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    {
+    rules: {
+      "react/no-unescaped-entities": "off",
+    },
+  }
   ]),
 ]);
 
