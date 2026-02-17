ef95a9170d9c065ced8a26047cadf3ac89a6b13b
SheetalTaank12 <sheetaltaank74@gmail.com>
2026-02-17 23:43:06 +0530

    add

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:8090";
+export const BASE_URL = "https://linklix.onrender.com";
 
 export const clientServer = axios.create({
   baseURL: BASE_URL,
